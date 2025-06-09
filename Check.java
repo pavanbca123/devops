49cfffe295d13c4f892259598893bd0da880e2e4
pavanbca123 <pavanstaish201@gmail.com>
2025-06-09 06:00:48 -0400

    save

--- Check.java
+++ Check.java
@@ -1,20 +1,18 @@
-public class Check{
-    public string checker(int num){
-        if (num>0){
-            return"positive";
-        }
-        else if(num<0){
-            return"Negative";
-        }
-        else{
-            return"Zero";
+public class Check {
+    public String checker(int num) {
+        if (num > 0) {
+            return "positive";
+        } else if (num < 0) {
+            return "negative";
+        } else {
+            return "Zero";
         }
     }
-    public static void main(String[] args) {
-        Check c1=new Check();
-        System.out.println("number is:"+c1.checker(20));
-        System.out.println("number is:"+c1.checker(-1));
-        System.out.println("number is:"+c1.checker(0));
 
+    public static void main(String[] args) {
+        Check c1 = new Check();
+        System.out.println("number is: " + c1.checker(20));
+        System.out.println("number is: " + c1.checker(-1));
+        System.out.println("number is: " + c1.checker(0));
     }
 }
